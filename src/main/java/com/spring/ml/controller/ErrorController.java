6545f736cefd1d4ebe0691292d8d87b37743397a
ShipJH <ShipJH@CARROT101-PC>
2019-07-03 13:54:39 +0900

    error edit

--- src/main/java/com/spring/ml/controller/ErrorController.java
+++ src/main/java/com/spring/ml/controller/ErrorController.java
@@ -26,22 +26,13 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 		
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-		log.info(status.toString());
-		log.info(String.valueOf(status));
-		log.info(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
-		log.info(HttpStatus.INTERNAL_SERVER_ERROR.toString());
-		log.info(HttpStatus.NOT_FOUND.toString());
-		//TODO: 벨류로 리턴받아오자 ( enum 클래스가 변경 된 것 같다.. ) 그다음에, valueOf 하자. 
+		log.debug(status.toString());
 		
-		
-		if (String.valueOf(status).equalsIgnoreCase(HttpStatus.NOT_FOUND.toString())) {
+		if(String.valueOf(status).equals(String.valueOf(HttpStatus.NOT_FOUND.value()))) {
 			return "views/errors/error404";
-		} else if (String.valueOf(status).equalsIgnoreCase(HttpStatus.INTERNAL_SERVER_ERROR.toString())) {
-			return "views/errors/error500";
+		}else if(String.valueOf(status).equals(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))) {
+			return "views/errors/error500";			
 		}
-
-		
-		
 		
 		return "views/errors/error500";
 		
